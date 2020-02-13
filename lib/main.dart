a22252227c8f9ff65d8b52b5f6cfe982115e995f
Md Rafat Jamader Maraz <razhossain201@gmail.com>
2020-02-13 22:19:11 +0600

    App Icon Label changed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
@@ -6,7 +7,12 @@ void main() => runApp(BMICalculator());
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.light().copyWith(
         primaryColor: Color(0xFFFFFFFF),
         scaffoldBackgroundColor: Color(0xFFFFFFFF),
